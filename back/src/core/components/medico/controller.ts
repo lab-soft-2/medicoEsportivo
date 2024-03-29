9364449ee9db6133cf99b980c43cc0f4e9685633
Vitor-chng <63964921+Vitor-chng@users.noreply.github.com>
2024-03-29 14:02:00 -0300

    a

--- back/src/core/components/medico/controller.ts
+++ back/src/core/components/medico/controller.ts
@@ -177,7 +177,7 @@ export class MedicoController {
         request.write(data)
         request.end();
 
-        return res.status(200).json({ "Documento criado": result })
+        return res.status(200).json({ "Documento criado": 'result' })
     }
 
     static async updatePaciente(req: Request, res: Response) {
